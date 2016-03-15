c89e3520b765c595db3a6ffbc491ade5df2c335f
Manuel Vacelet <manuel.vacelet@enalean.com>
2016-03-15 19:06:41 +0100

    No longer need specific code for services activation
    Already done by setup.sh in std

--- boot-postinstall.sh
+++ boot-postinstall.sh
@@ -3,6 +3,3 @@
 set -e
 
 # Place here your specific configuration at install time
-/usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/tools/utils/admin/activate_plugin.php cardwall
-/usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/tools/utils/admin/activate_plugin.php agiledashboard
-/usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/tools/utils/admin/activate_plugin.php git
