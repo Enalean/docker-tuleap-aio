Enalean/docker-tuleap-aio

commit c0a3ce867bada5905ec92d11461a30dd61a6cf41

request #9974: Default self signed certificate used by Apache is the same for all the containers using the same image
The keys are now generated on the start of the container with an empty data volume

boot-install.sh
@@ -42,6 +42,19 @@ echo "root: $root_passwd" >> /root/.tuleap_passwd
 # Force the generation of the SSH host keys
 service sshd start && service sshd stop
 
+# Generate self signed certificate for Apache
+cat << EOF | openssl req -new -nodes -keyout /etc/pki/tls/private/localhost.key \
+         -x509 -sha256 -days 365 -set_serial $RANDOM -extensions v3_req \
+         -out /etc/ssl/certs/localhost.crt 2>/dev/null
+--
+SomeState
+SomeCity
+SomeOrganization
+SomeOrganizationalUnit
+${VIRTUAL_HOST}
+root@${VIRTUAL_HOST}
+EOF
+
 # Place for post install stuff
 ./boot-postinstall.sh
 

Dockerfile
@@ -51,7 +51,8 @@ RUN sed -i '/session    required     pam_loginuid.so/c\#session    required     
     tuleap-api-explorer && \
     yum clean all && \
     sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config && \
-    rm -f /etc/ssh/ssh_host_*
+    rm -f /etc/ssh/ssh_host_* && \
+    rm -f /etc/ssl/certs/localhost.crt /etc/pki/tls/private/localhost.key
 
 COPY supervisord.conf /etc/supervisord.conf
 

commit 71ea5ede0a2679296d8c4b0f8eea4fcc6b743663

Tuleap can be restarted with a new container with existing data
Commit c0a3ce867bada has introduced the issue. The newly created
certificate is removed and not moved to the persistent data.

boot-fixpath.sh
@@ -48,6 +48,12 @@ ln -s /data/etc/passwd passwd
 ln -s /data/etc/shadow shadow
 ln -s /data/etc/group group
 
+cd /etc/pki/tls/private
+ln -s /data/etc/pki/tls/private/localhost.key localhost.key
+
+cd /etc/ssl/certs
+ln -s /data/etc/ssl/certs/localhost.crt localhost.crt
+
 cd /etc/logrotate.d
 ln -s /data/etc/logrotate.d/httpd httpd
 

boot-install.sh
@@ -11,6 +11,8 @@ mkdir -p /data/etc/ssh/
 mkdir -p /data/home
 mkdir -p /data/lib
 mkdir -p /data/etc/logrotate.d
+mkdir -p /data/etc/ssl/certs/
+mkdir -p /data/etc/pki/tls/private/
 mkdir -p /data/root && chmod 700 /data/root
 
 pushd . > /dev/null
@@ -77,6 +79,8 @@ service crond stop
 # Conf
 mv /etc/httpd/conf            /data/etc/httpd
 mv /etc/httpd/conf.d          /data/etc/httpd
+mv /etc/pki/tls/private/localhost.key /data/etc/pki/tls/private
+mv /etc/ssl/certs/localhost.crt /data/etc/ssl/certs
 mv /etc/tuleap                /data/etc
 mv /etc/aliases               /data/etc
 mv /etc/logrotate.d/httpd     /data/etc/logrotate.d