a87115d0efcf7a070952447acd147952b6e5a3cc
Manuel VACELET <manuel.vacelet@enalean.com>
2019-05-17 15:58:31 +0200

    Ensure mysql is properly started and shut down

--- run.sh
+++ run.sh
@@ -8,12 +8,13 @@ set -o pipefail
 if [ ! -d /var/opt/rh/rh-mysql57/lib/mysql/mysql ]; then
     /usr/local/bin/mysql-init.sh
 
-    sudo -u mysql /opt/rh/rh-mysql57/root/usr/libexec/mysqld-scl-helper enable rh-mysql57 -- /opt/rh/rh-mysql57/root/usr/libexec/mysqld --basedir=/opt/rh/rh-mysql57/root/usr --pid-file=/var/run/rh-mysql57-mysqld/mysqld.pid &
-
     sed -i -e 's%/usr/bin/systemctl%/usr/bin/true%' /usr/share/tuleap/tools/setup/el7/include/define.sh
     /usr/share/tuleap/tools/setup.el7.sh -y --server-name=tuleap.local --mysql-server=localhost --mysql-password=${MYSQL_ROOT_PASSWORD}
 
     scl enable rh-mysql57 -- mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+    while kill -0 $(cat /var/run/rh-mysql57-mysqld/mysqld.pid); do
+        sleep 1
+    done
 fi
 
 exec /usr/bin/supervisord --nodaemon --configuration /etc/supervisord.conf
